d59a08e7d62708f42dc463ee54546892d2d37d27
Lala Tseng <z162614@gmail.com>
2025-03-13 14:38:14 +0800

    Add page reading time feature.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import mdx from '@astrojs/mdx';
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://johnson-dev.netlify.app",
   integrations: [tailwind(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
